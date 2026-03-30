c80cfb4d8d3b1aad2724ec523790805daa535c20
martynofotoraw-cmd <martyno.foto.raw@gmail.com>
2026-03-30 11:54:17 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,5 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/bash
 
-mkdir -p /var/data/backups
+mkdir -p backups
 
-if [ ! -f "/var/data/database.db" ] && [ -f "./database.db" ]; then
-  echo "Kopijuoju pradine database.db i persistent disk..."
-  cp ./database.db /var/data/database.db
-fi
-
-chmod 664 /var/data/database.db || true
-
-exec gunicorn --bind 0.0.0.0:${PORT:-10000} app:app
+gunicorn app:app
